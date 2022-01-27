cabebb21093980364d9be492ec68bbf1e7dd193b
Andrei Dzebysh <presnia@yahoo.com>
2022-01-27 11:04:41 +0300

    Added self-made components as well as the button and the input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import {useState} from "react";
+import PostList from "./components/PostList/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import './App.css';
+
+
 
 function App() {
+  const [posts, setPosts] = useState([
+      {id: 1, title: 'Javascript 1', body: 'Javascript - programming language'},
+      {id: 2, title: 'Javascript 2', body: 'Javascript - programming language'},
+      {id: 3, title: 'Javascript 3', body: 'Javascript - programming language'}
+  ]);
 
   return (
     <div className="App">
         <Counter/>
         <ClassCounter/>
+        <form>
+            <MyInput type="text" placeholder='Post name'/>
+            <MyInput type="text" placeholder='Post description'/>
+            <MyButton>CREATE POST</MyButton>
+        </form>
+        <PostList posts={posts} title='Posts About JS'/>
     </div>
   );
 }
---
+++ src/components/PostItem/PostItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './PostItem.css';
+
+const PostItem = (props) => {
+    return (
+        <div className="post">
+            <div className="post-content">
+                <strong>{props.post.id}. {props.post.title}</strong>
+                <div>{props.post.body}</div>
+            </div>
+            <div className="post-buttons">
+                <button>Delete</button>
+            </div>
+        </div>
+    );
+};
+
+export default PostItem;
