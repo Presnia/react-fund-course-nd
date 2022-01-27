2726e460cc204057855f24394809574c3753d5c8
Andrei Dzebysh <presnia@yahoo.com>
2022-01-27 13:24:33 +0300

    Data exchange between components. From parent to child and vice versa

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import {useState} from "react";
 import PostList from "./components/PostList/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm/PostForm";
 import './App.css';
-
-
 
 function App() {
   const [posts, setPosts] = useState([
@@ -15,40 +12,15 @@ function App() {
       {id: 3, title: 'Javascript 3', body: 'Javascript - programming language'}
   ]);
 
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-
-  const addNewPost = (e) => {
-      e.preventDefault();
-      const newPost = {
-          id: Date.now(),
-          title,
-          body
-      }
+  const createPost = (newPost) => {
       setPosts([...posts, newPost]);
-      setTitle('');
-      setBody('');
   }
 
   return (
     <div className="App">
         <Counter/>
         <ClassCounter/>
-        <form>
-            {/*Управляемый компонент*/}
-            <MyInput value={title}
-                     onChange={e => setTitle(e.target.value)}
-                     type="text"
-                     placeholder='Post name'
-            />
-            {/*Неуправляемый компонент*/}
-            <MyInput
-                value={body}
-                onChange={e => setBody(e.target.value)}
-                type="text"
-                placeholder='Post description'/>
-            <MyButton onClick={addNewPost}>CREATE POST</MyButton>
-        </form>
+        <PostForm create={createPost}/>
         <PostList posts={posts} title='Posts About JS'/>
     </div>
   );
