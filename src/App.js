e79e41a35fa86ecf1945826830e74a7988f9212a
Andrei Dzebysh <presnia@yahoo.com>
2022-01-27 11:31:10 +0300

    Managed and unmanaged components has been created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import PostList from "./components/PostList/PostList";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
@@ -15,14 +15,32 @@ function App() {
       {id: 3, title: 'Javascript 3', body: 'Javascript - programming language'}
   ]);
 
+  const [title, setTitle] = useState('');
+
+  const bodyInputRef = useRef();
+
+  const addNewPost = (e) => {
+      e.preventDefault();
+      console.log(bodyInputRef.current.value);
+      console.log(title);
+  }
+
   return (
     <div className="App">
         <Counter/>
         <ClassCounter/>
         <form>
-            <MyInput type="text" placeholder='Post name'/>
-            <MyInput type="text" placeholder='Post description'/>
-            <MyButton>CREATE POST</MyButton>
+            {/*Управляемый компонент*/}
+            <MyInput value={title}
+                     onChange={e => setTitle(e.target.value)}
+                     type="text"
+                     placeholder='Post name'
+            />
+            {/*Неуправляемый компонент*/}
+            <MyInput ref={bodyInputRef}
+                     type="text"
+                     placeholder='Post description'/>
+            <MyButton onClick={addNewPost}>CREATE POST</MyButton>
         </form>
         <PostList posts={posts} title='Posts About JS'/>
     </div>
