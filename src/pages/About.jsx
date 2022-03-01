008227663e2980803249f0b862631cdfb8cdbcd7
Andrei Dzebysh <presnia@yahoo.com>
2022-03-01 12:01:45 +0300

    Routing has been implemented

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            This application is created as a training project
+        </h1>
+    );
+};
+
+export default About;
